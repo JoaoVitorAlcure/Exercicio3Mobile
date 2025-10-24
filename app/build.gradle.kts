5f39134f08af7f3d1a2a21f0f29f1ffe6e1aa6d9
20231tiimi0154 <joaovitoralcure@gmail.com>
2025-10-24 08:14:35 -0300

    Changes in the build.gradle.kts (app/task)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")
     alias(libs.plugins.kotlin.android)
     id("kotlin-parcelize")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,6 +42,7 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
